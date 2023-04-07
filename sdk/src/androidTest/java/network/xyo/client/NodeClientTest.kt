11057d959e68e70daff60641005cd9a991b5056b
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-04-07 16:05:36 -0400

    tests for passing payloads and query

--- sdk/src/androidTest/java/network/xyo/client/NodeClientTest.kt
+++ sdk/src/androidTest/java/network/xyo/client/NodeClientTest.kt
@@ -7,6 +7,8 @@ import network.xyo.client.payload.XyoPayload
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 
+class GetPayload(val hashes: MutableList<String>): XyoPayload("network.xyo.query.archivist.get") {}
+
 class NodeClientTest {
 
     val apiDomainLocal = "http://10.0.2.2:8080"
@@ -15,24 +17,54 @@ class NodeClientTest {
     fun DiscoverTest() {
         val account = XyoAccount()
         val client = NodeClient(apiDomainLocal, account)
-        val payload = XyoPayload("network.xyo.query.module.discover")
+        val query = XyoPayload("network.xyo.query.module.discover")
 
         runBlocking {
-            val postResult = client.query(payload, null)
+            val postResult = client.query(query, null, null)
             assertEquals(null, postResult.errors)
         }
     }
 
     @Test
-    fun AddressTest() {
+    fun ArchivistInsertTest() {
+        val hash1 = "7ad2100faa86dd870898e61707786bfac3181bd1ce36ac31e227371060f8456c"
+        val hash2 = "3da33603417622f4cdad2becbca8c7889623d9045d0e8923e1702a99d2f3e47c"
+
         val account = XyoAccount()
         val client = NodeClient("$apiDomainLocal/Archivist", account)
-        val payload = XyoPayload("network.xyo.query.module.discover")
+
+        val query = XyoPayload("network.xyo.query.archivist.insert")
+        val payloads = mutableListOf<XyoPayload>()
+        payloads.add(XyoPayload("network.xyo.payload"))
 
         runBlocking {
-            val address = XyoAccount()
-            val postResult = client.query(payload, null)
+            val postResult = client.query(query, payloads, null)
             assertNotEquals(postResult.response, null)
+
+            val response = postResult.response
+            if (response != null) {
+                assertTrue(response.contains(hash1))
+                assertTrue(response.contains(hash2))
+            } else {
+                throw(Error("Response should not be null"))
+            }
+        }
+
+        val hashes = mutableListOf<String>()
+        hashes.add(hash2)
+        val getQueryPayload = GetPayload(hashes)
+
+
+        runBlocking {
+            val postResult = client.query(getQueryPayload, null, null)
+            assertNotEquals(postResult.response, null)
+
+            val response = postResult.response
+            if (response != null) {
+                assertTrue(response.contains("network.xyo.payload"))
+            } else {
+                throw(Error("Response should not be null"))
+            }
         }
     }
 }
