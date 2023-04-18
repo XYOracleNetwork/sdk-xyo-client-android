86e9055e03e3c34f8bbb556cf8a98c2fe6693b2a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-04-18 14:01:50 -0400

    panel tests now use nodeClient

--- sdk/src/androidTest/java/network/xyo/client/XyoPanelTest.kt
+++ sdk/src/androidTest/java/network/xyo/client/XyoPanelTest.kt
@@ -5,6 +5,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import kotlinx.coroutines.runBlocking
 import network.xyo.client.address.XyoAccount
+import network.xyo.client.node.client.NodeClient
 import network.xyo.client.payload.XyoPayload
 import network.xyo.client.witness.system.info.XyoSystemInfoWitness
 import org.junit.Before
@@ -19,8 +20,8 @@ class XyoPanelTest {
 
     lateinit var appContext: Context
 
-    val apiDomainBeta = "https://beta.api.archivist.xyo.network"
-    val apiDomainLocal = "http://10.0.2.2:8080"
+    val apiDomainBeta = "https://beta.api.archivist.xyo.network/Archivist"
+    val apiDomainLocal = "http://10.0.2.2:8080/Archivist"
     val archive = "temp"
 
     @Before
@@ -29,11 +30,11 @@ class XyoPanelTest {
         this.appContext = InstrumentationRegistry.getInstrumentation().targetContext
     }
 
-    fun testCreatePanel(apiDomain: String) {
-        val address = XyoAccount()
-        val witness = XyoWitness<XyoPayload>(address)
-        val panel = XyoPanel(appContext, archive, apiDomain, listOf(witness))
-        assertNotNull(address)
+    fun testCreatePanel(nodeUrl: String) {
+        val witness = XyoWitness<XyoPayload>(XyoAccount())
+        val clients = arrayListOf(NodeClient(nodeUrl, XyoAccount()))
+        val panel = XyoPanel(appContext, clients, listOf(witness))
+        assertNotNull(clients)
         assertNotNull(panel)
     }
 
@@ -47,16 +48,15 @@ class XyoPanelTest {
         testCreatePanel(apiDomainLocal)
     }
 
-    fun testPanelReport(apiDomain: String) {
+    fun testPanelReport(nodeUrl: String) {
         runBlocking {
-            val apiDomain = apiDomain
-            val archive = archive
-            val address = XyoAccount(XyoSerializable.hexToBytes("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"))
-            val address2 = XyoAccount(XyoSerializable.hexToBytes("5a95531488b4d0d3645aea49678297ae9e2034879ce0389b80eb788e8b533592"))
-            val witness = XyoWitness(address, fun(context: Context, previousHash: String?): XyoPayload {
+            val clients = arrayListOf(NodeClient(nodeUrl, XyoAccount()))
+            val witnessAccount = XyoAccount(XyoSerializable.hexToBytes("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"))
+            val witness2Account = XyoAccount(XyoSerializable.hexToBytes("5a95531488b4d0d3645aea49678297ae9e2034879ce0389b80eb788e8b533592"))
+            val witness = XyoWitness(witnessAccount, fun(context: Context, previousHash: String?): XyoPayload {
                 return XyoPayload("network.xyo.basic", previousHash)
             })
-            val panel = XyoPanel(appContext, archive, apiDomain, listOf(witness, XyoSystemInfoWitness(address2)))
+            val panel = XyoPanel(appContext, clients, listOf(witness, XyoSystemInfoWitness(witness2Account)))
             val result = panel.reportAsync()
             result.apiResults.forEach {
                 assertEquals(it.errors, null)
@@ -77,8 +77,9 @@ class XyoPanelTest {
     @Test
     fun testSimplePanelReport() {
         runBlocking {
-            val panel = XyoPanel(appContext, fun(_context:Context, previousHash: String?): XyoEventPayload {
-                return XyoEventPayload("test_event", previousHash)
+            val clients = arrayListOf(NodeClient(apiDomainBeta, XyoAccount()))
+            val panel = XyoPanel(appContext, clients, fun(_context:Context, previousHash: String?): XyoEventPayload {
+                return XyoEventPayload("test_event", null)
             })
             val result = panel.reportAsync()
             result.apiResults.forEach { assertEquals(it.errors, null) }
@@ -88,7 +89,7 @@ class XyoPanelTest {
     @Test
     fun testReportEvent() {
         runBlocking {
-            val panel = XyoPanel(appContext, null, null, listOf(XyoSystemInfoWitness()))
+            val panel = XyoPanel(appContext, apiDomainBeta, XyoAccount(), listOf(XyoSystemInfoWitness()))
             val result = panel.reportAsync()
             result.apiResults.forEach { assertEquals(it.errors, null) }
         }
