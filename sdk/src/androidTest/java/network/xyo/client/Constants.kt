20edc356dce2fea22d4a4926b93bcbe36c1e08b0
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-04-18 16:23:00 -0400

    add api domains to constants

--- sdk/src/androidTest/java/network/xyo/client/Constants.kt
+++ sdk/src/androidTest/java/network/xyo/client/Constants.kt
@@ -5,11 +5,13 @@ import network.xyo.client.payload.XyoPayload
 
 class DebugPayload(schema: String, val nonce: Int) : XyoPayload(schema) {}
 
-class PayloadTestConstants {
+class TestConstants {
     companion object {
         val accountPrivateKey = "69f0b123c094c34191f22c25426036d6e46d5e1fab0a04a164b3c1c2621152ab"
         val TestAccount = XyoAccount(XyoSerializable.hexToBytes(accountPrivateKey))
         val debugPayload = DebugPayload("network.xyo.debug",1)
         val debugPayloadHash = "15b8d0e30ca5aa96ca6cc9e1528c075aec88cd3f2c3eb0394fde647eb4bf4547"
+        val nodeUrlLocal = "http://10.0.2.2:8080"
+        val nodeUrlBeta = "https://beta.api.archivist.xyo.network"
     }
 }
