7c0e0cff8212e1457276d215b3e03ed71eb08325
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-04-18 16:13:18 -0400

    extract test values

---
+++ sdk/src/androidTest/java/network/xyo/client/Constants.kt
@@ -0,0 +1,15 @@
+package network.xyo.client
+
+import network.xyo.client.address.XyoAccount
+import network.xyo.client.payload.XyoPayload
+
+class DebugPayload(schema: String, val nonce: Int) : XyoPayload(schema) {}
+
+class PayloadTestConstants {
+    companion object {
+        val accountPrivateKey = "69f0b123c094c34191f22c25426036d6e46d5e1fab0a04a164b3c1c2621152ab"
+        val TestAccount = XyoAccount(XyoSerializable.hexToBytes(accountPrivateKey))
+        val debugPayload = DebugPayload("network.xyo.debug",1)
+        val debugPayloadHash = "15b8d0e30ca5aa96ca6cc9e1528c075aec88cd3f2c3eb0394fde647eb4bf4547"
+    }
+}
