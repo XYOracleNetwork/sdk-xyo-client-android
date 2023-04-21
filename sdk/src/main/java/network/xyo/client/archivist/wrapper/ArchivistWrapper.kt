782784f4448ac25928d62683e1069d1ffbf89e4b
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-04-21 15:02:09 -0400

    lint fixes

--- sdk/src/main/java/network/xyo/client/archivist/wrapper/ArchivistWrapper.kt
+++ sdk/src/main/java/network/xyo/client/archivist/wrapper/ArchivistWrapper.kt
@@ -2,6 +2,7 @@ package network.xyo.client.archivist.wrapper
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import network.xyo.client.XyoSerializable
 import network.xyo.client.address.XyoAccount
 import network.xyo.client.node.client.NodeClient
@@ -9,12 +10,14 @@ import network.xyo.client.node.client.PostQueryResult
 import network.xyo.client.payload.XyoPayload
 
 open class ArchivistWrapper(private val nodeClient: NodeClient) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @RequiresApi(Build.VERSION_CODES.M)
     suspend fun get(hashes: List<String>, previousHash: String?): PostQueryResult {
         val query = ArchivistGetQueryPayload(hashes)
         return nodeClient.query(query, null, previousHash)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @RequiresApi(Build.VERSION_CODES.M)
     suspend fun insert(payloads: List<XyoPayload>, previousHash: String?): PostQueryResult {
         val payloadHashes = arrayListOf<String>()
