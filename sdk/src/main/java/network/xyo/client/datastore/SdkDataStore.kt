03fe46f0ecddf92851eede7be21f7a6d521fcdab
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-11 16:56:54 -0400

    sdk prefs serializer

---
+++ sdk/src/main/java/network/xyo/client/datastore/SdkDataStore.kt
@@ -0,0 +1,21 @@
+package network.xyo.client.datastore
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import com.network.xyo.client.data.PrefsDataStoreProtos.PrefsDataStore
+import java.io.InputStream
+import java.io.OutputStream
+
+object SdkDataStoreSerializer : Serializer<PrefsDataStore> {
+    override val defaultValue: PrefsDataStore = PrefsDataStore.getDefaultInstance()
+    override suspend fun readFrom(input: InputStream): PrefsDataStore {
+        try {
+            return PrefsDataStore.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: PrefsDataStore, output: OutputStream) = t.writeTo(output)
+}
