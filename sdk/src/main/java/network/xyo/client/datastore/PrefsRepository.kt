4d678b36317df479c016a09a4baf32e2eb42dc8e
Matt Jones <885417+jonesmac@users.noreply.github.com>
2023-05-12 00:02:10 -0400

    working get, set, and clear methods

--- sdk/src/main/java/network/xyo/client/datastore/PrefsRepository.kt
+++ sdk/src/main/java/network/xyo/client/datastore/PrefsRepository.kt
@@ -1,12 +1,20 @@
 package network.xyo.client.datastore
 
 import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.dataStore
 import com.network.xyo.client.data.PrefsDataStoreProtos.PrefsDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import network.xyo.client.address.XyoAccount
+import network.xyo.client.xyoScope
 
 const val DATA_STORE_FILE_NAME = "prefs.pb"
 
@@ -26,17 +34,40 @@ class PrefsRepository {
         this.prefsDataStore = context.prefsDataStore
     }
 
-    fun getAccountKey(): Flow<String> {
-        return this.prefsDataStore.data.map { prefs ->
-            prefs.accountKey
+    @RequiresApi(Build.VERSION_CODES.M)
+    suspend fun getAccountKey(): String {
+        val savedKey = prefsDataStore.data.first().accountKey
+        Log.d("xyoClient", "savedKey: $savedKey")
+        return if (savedKey.isEmpty()) {
+            val newAccount = XyoAccount()
+            setAccountKey(newAccount.private.hex)
+            newAccount.private.hex
+        } else {
+            return savedKey
         }
     }
 
-    suspend fun setAccountKey(accountKey: String): PrefsDataStore {
-        return this.prefsDataStore.updateData { currentPrefs ->
-            currentPrefs.toBuilder()
-                .setAccountKey(accountKey)
-            currentPrefs
+    private suspend fun setAccountKey(accountKey: String): DataStore<PrefsDataStore> {
+        val job = xyoScope.launch {
+            this@PrefsRepository.prefsDataStore.updateData { currentPrefs ->
+                currentPrefs.toBuilder()
+                    .setAccountKey(accountKey)
+                    .build()
+            }
         }
+        job.join()
+        return prefsDataStore
+    }
+
+    suspend fun clearSavedAccountKey(): DataStore<PrefsDataStore> {
+        val job = xyoScope.launch {
+            this@PrefsRepository.prefsDataStore.updateData { currentPrefs ->
+                currentPrefs.toBuilder()
+                    .setAccountKey("")
+                    .build()
+            }
+        }
+        job.join()
+        return prefsDataStore
     }
 }
