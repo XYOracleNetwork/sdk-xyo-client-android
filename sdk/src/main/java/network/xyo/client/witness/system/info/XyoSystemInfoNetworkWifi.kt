398683e362cdf99cd198fe7e22b12412f4f5df05
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-04 15:46:44 -0500

    test for system info witness

--- sdk/src/main/java/network/xyo/client/witness/system/info/XyoSystemInfoNetworkWifi.kt
+++ sdk/src/main/java/network/xyo/client/witness/system/info/XyoSystemInfoNetworkWifi.kt
@@ -8,7 +8,6 @@ import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.net.wifi.WifiInfo
-import android.net.wifi.WifiManager
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.squareup.moshi.JsonClass
