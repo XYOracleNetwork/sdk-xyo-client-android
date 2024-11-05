13699366c101ba725c350bb8c5fe461e31362917
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-05 15:20:58 -0500

    remove unnecessary decorator

--- sdk/src/main/java/network/xyo/client/archivist/wrapper/ArchivistGetQueryPayload.kt
+++ sdk/src/main/java/network/xyo/client/archivist/wrapper/ArchivistGetQueryPayload.kt
@@ -1,7 +1,5 @@
 package network.xyo.client.archivist.wrapper
 
-import com.squareup.moshi.JsonClass
 import network.xyo.client.payload.XyoPayload
 
-@JsonClass(generateAdapter = true)
 open class ArchivistGetQueryPayload(val hashes: List<String>, schema: String = "network.xyo.query.archivist.get"): XyoPayload(schema)
