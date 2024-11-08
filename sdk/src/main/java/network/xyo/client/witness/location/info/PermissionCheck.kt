0441a8d202e6500a2f7bf5f0ec0412402de5568f
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-08 13:35:00 -0500

    add class to check permissions

---
+++ sdk/src/main/java/network/xyo/client/witness/location/info/PermissionCheck.kt
@@ -0,0 +1,35 @@
+package network.xyo.client.witness.location.info
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.GoogleApiAvailability
+
+class LocationPermissions {
+    companion object {
+        fun check(context: Context): Boolean {
+            val gpInstance = GoogleApiAvailability.getInstance()
+            val available = gpInstance.isGooglePlayServicesAvailable(context)
+            val googlePlayServicesAvailable = available == 1
+            if (googlePlayServicesAvailable) {
+                Log.e("xyoClient", "Google Play Service not installed")
+                return false
+            }
+
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                Log.e("xyoClient", "ACCESS_FINE_LOCATION permission not allowed")
+                return false
+            }
+
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                Log.e("xyoClient", "ACCESS_FINE_LOCATION permission not allowed")
+                return false
+            }
+            return true
+        }
+    }
+}
--- sdk/src/main/java/network/xyo/client/witness/location/info/XyoLocationPayload.kt
+++ sdk/src/main/java/network/xyo/client/witness/location/info/XyoLocationPayload.kt
@@ -1,6 +1,7 @@
 package network.xyo.client.witness.location.info
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -38,68 +39,51 @@ class XyoLocationPayload (
 
     companion object {
 
+        @SuppressLint("MissingPermission")
         fun detect(context: Context): XyoLocationPayload? {
-            val gpInstance = GoogleApiAvailability.getInstance()
-            val available = gpInstance.isGooglePlayServicesAvailable(context)
-            val googlePlayServicesAvailable = available == 1
-            if (googlePlayServicesAvailable) {
-                Log.e("xyoClient", "Google Play Service not installed")
-                return null
-            }
+            if (LocationPermissions.check((context))) {
+                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+                var coordinates: Coordinates? = null
 
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-                Log.e("xyoClient", "ACCESS_FINE_LOCATION permission not allowed")
-                return null
-            }
+                try {
+                    val latch = CountDownLatch(1)
 
-            if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-                Log.e("xyoClient", "ACCESS_FINE_LOCATION permission not allowed")
-                return null
-            }
-
-            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-            var coordinates: Coordinates? = null
-
-            try {
-                val latch = CountDownLatch(1)
-
-                fusedLocationClient.lastLocation
-                    .addOnSuccessListener { location: Location? ->
-                        if (location != null) {
-                            Log.w("xyoClient", "Location was null")
-                            coordinates = Coordinates(
-                                location.accuracy,
-                                location.altitude,
-                                null,
-                                location.bearing,
-                                location.latitude,
-                                location.longitude,
-                                location.speed
-                            )
-                            // countDown to zero to lift the latch
-                            latch.countDown()
-                        } else {
-                            // countDown to zero to lift the latch
-                            latch.countDown()
-                            Log.e("xyoClient","Location not available")
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location: Location? ->
+                            if (location != null) {
+                                Log.w("xyoClient", "Location was null")
+                                coordinates = Coordinates(
+                                    location.accuracy,
+                                    location.altitude,
+                                    null,
+                                    location.bearing,
+                                    location.latitude,
+                                    location.longitude,
+                                    location.speed
+                                )
+                                // countDown to zero to lift the latch
+                                latch.countDown()
+                            } else {
+                                // countDown to zero to lift the latch
+                                latch.countDown()
+                                Log.e("xyoClient","Location not available")
+                            }
                         }
-                    }
-                    .addOnFailureListener {
-                        Log.e("xyoClient","Failed to get location: ${it.message}")
-                    }
-                // Wait for up to 5 seconds for the location
-                latch.await(5, TimeUnit.SECONDS)
+                        .addOnFailureListener {
+                            Log.e("xyoClient","Failed to get location: ${it.message}")
+                        }
+                    // Wait for up to 5 seconds for the location
+                    latch.await(5, TimeUnit.SECONDS)
 
-                if (coordinates == null) {
-                    return null
-                } else {
-                    val currentLocation = CurrentLocation(coordinates!!, System.currentTimeMillis())
-                    return XyoLocationPayload(currentLocation)
+                    if (coordinates == null) {
+                        return null
+                    } else {
+                        val currentLocation = CurrentLocation(coordinates!!, System.currentTimeMillis())
+                        return XyoLocationPayload(currentLocation)
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
             }
             return null
 
