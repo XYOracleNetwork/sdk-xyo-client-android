e19185c82e0bc4d79b14c07fd072b8276f7fbe47
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-08 14:13:27 -0500

    check for google play services

--- sdk/src/main/java/network/xyo/client/witness/location/info/PermissionCheck.kt
+++ sdk/src/main/java/network/xyo/client/witness/location/info/PermissionCheck.kt
@@ -10,14 +10,6 @@ import com.google.android.gms.common.GoogleApiAvailability
 class LocationPermissions {
     companion object {
         fun check(context: Context): Boolean {
-            val gpInstance = GoogleApiAvailability.getInstance()
-            val available = gpInstance.isGooglePlayServicesAvailable(context)
-            val googlePlayServicesAvailable = available == 1
-            if (googlePlayServicesAvailable) {
-                Log.e("xyoClient", "Google Play Service not installed")
-                return false
-            }
-
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
                 Log.e("xyoClient", "ACCESS_FINE_LOCATION permission not allowed")
@@ -27,6 +19,17 @@ class LocationPermissions {
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
                 Log.e("xyoClient", "ACCESS_FINE_LOCATION permission not allowed")
+                return false
+            }
+            return true
+        }
+
+        fun checkGooglePlayServices(context: Context): Boolean {
+            val gpInstance = GoogleApiAvailability.getInstance()
+            val available = gpInstance.isGooglePlayServicesAvailable(context)
+            val googlePlayServicesAvailable = available == 1
+            if (googlePlayServicesAvailable) {
+                Log.e("xyoClient", "Google Play Service not installed")
                 return false
             }
             return true
