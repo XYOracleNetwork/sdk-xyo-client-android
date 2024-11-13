86c09e971afc092b61e31c44d9dfa429792fa682
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-13 11:59:09 -0500

    user MultiProcessDataStoreFactory since we witness on a different thread

--- sdk/src/main/java/network/xyo/client/datastore/XyoAccountDataStore.kt
+++ sdk/src/main/java/network/xyo/client/datastore/XyoAccountDataStore.kt
@@ -2,7 +2,7 @@ package network.xyo.client.datastore
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.core.MultiProcessDataStoreFactory
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +15,7 @@ val defaults = DefaultXyoSdkSettings()
 fun Context.xyoAccountDataStore(name: String? = defaults.accountPreferences.fileName, path: String? = defaults.accountPreferences.storagePath): DataStore<PrefsDataStore> {
     val dataStoreFile = File(filesDir, "$path/$name")
 
-    return DataStoreFactory.create(
+    return MultiProcessDataStoreFactory.create(
         serializer = PrefsDataStoreSerializer,
         produceFile = { dataStoreFile },
         corruptionHandler = ReplaceFileCorruptionHandler(
