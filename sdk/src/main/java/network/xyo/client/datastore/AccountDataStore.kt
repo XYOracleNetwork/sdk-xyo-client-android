d1510a24d16e3b49ec4a4033c31af89cf40d9e5a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-18 16:28:17 -0500

    use new defaults import

--- sdk/src/main/java/network/xyo/client/datastore/AccountDataStore.kt
+++ sdk/src/main/java/network/xyo/client/datastore/AccountDataStore.kt
@@ -6,15 +6,13 @@ import androidx.datastore.core.MultiProcessDataStoreFactory
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import network.xyo.client.settings.DefaultXyoSdkSettings
+import network.xyo.client.settings.defaultXyoSdkSettings
 import network.xyo.data.AccountPrefsDataStoreProtos.AccountPrefsDataStore
 import java.io.File
 
-val defaults = DefaultXyoSdkSettings()
-
 fun Context.xyoAccountDataStore(name: String?, path: String?): DataStore<AccountPrefsDataStore> {
-    val resolvedName = name ?: defaults.accountPreferences.fileName
-    val resolvedPath = path ?: defaults.accountPreferences.storagePath
+    val resolvedName = name ?: defaultXyoSdkSettings.accountPreferences.fileName
+    val resolvedPath = path ?: defaultXyoSdkSettings.accountPreferences.storagePath
 
     val dataStoreFile = File(filesDir, "$resolvedPath/$resolvedName")
 
--- sdk/src/main/java/network/xyo/client/settings/SettingsInterface.kt
+++ sdk/src/main/java/network/xyo/client/settings/SettingsInterface.kt
@@ -2,9 +2,14 @@ package network.xyo.client.settings
 
 interface SettingsInterface {
     val accountPreferences: AccountPreferences
+    val previousHashStorePreferences: PreviousHashStorePreferences
 }
 
-interface AccountPreferences {
+interface StorageLocationPreference {
     val fileName: String?
     val storagePath: String?
 }
+
+interface AccountPreferences: StorageLocationPreference {}
+
+interface PreviousHashStorePreferences: StorageLocationPreference {}
