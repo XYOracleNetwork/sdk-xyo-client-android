48434f52d6834281d3308d9dcf240f6f588df168
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-18 16:05:42 -0500

    Assume Previous Hash Store methods will use Coroutines

--- sdk/src/main/java/network/xyo/client/account/model/PreviousHashStore.kt
+++ sdk/src/main/java/network/xyo/client/account/model/PreviousHashStore.kt
@@ -1,7 +1,7 @@
 package network.xyo.client.account.model
 
 interface PreviousHashStore {
-    fun getItem(address: ByteArray): ByteArray?
-    fun removeItem(address: ByteArray)
-    fun setItem(address: ByteArray, previousHash: ByteArray)
+    suspend fun getItem(address: ByteArray): ByteArray?
+    suspend fun removeItem(address: ByteArray)
+    suspend fun setItem(address: ByteArray, previousHash: ByteArray)
 }
