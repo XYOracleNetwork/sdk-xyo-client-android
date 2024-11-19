272f14935e7ffd6f29f28b4a31a631c1fbd98da3
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-18 20:57:34 -0500

    let PreviousHashStorePrefsRepository take full settings interface

--- sdk/src/main/java/network/xyo/client/datastore/previous_hash_store/PreviousHashStorePrefsRepository.kt
+++ sdk/src/main/java/network/xyo/client/datastore/previous_hash_store/PreviousHashStorePrefsRepository.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.launch
 import network.xyo.client.account.hexStringToByteArray
 import network.xyo.client.account.model.PreviousHashStore
 import network.xyo.client.settings.PreviousHashStorePreferences
+import network.xyo.client.settings.SettingsInterface
 import network.xyo.client.settings.defaultXyoSdkSettings
 import network.xyo.client.xyoScope
 import network.xyo.data.PreviousHashPrefsDataStoreProtos.PreviousHashPrefsDataStore
@@ -14,19 +15,16 @@ import network.xyo.data.PreviousHashPrefsDataStoreProtos.PreviousHashPrefsDataSt
 
 class PreviousHashStorePrefsRepository(
     context: Context,
-    private val _previousHashStorePreferences: PreviousHashStorePreferences = defaultXyoSdkSettings.previousHashStorePreferences
+    settings: SettingsInterface = defaultXyoSdkSettings
 ): PreviousHashStore {
     private val appContext = context.applicationContext
+    private val previousHashStorePreferences: PreviousHashStorePreferences = settings.previousHashStorePreferences
 
     // This should set the proper paths for the prefs datastore each time the the class is instantiated
     @Volatile
     private var previousHashStorePrefsDataStore: DataStore<PreviousHashPrefsDataStore> = appContext.xyoPreviousHashDataStore(
         previousHashStorePreferences.fileName, previousHashStorePreferences.storagePath
     )
-
-    // Exposing as a getter so path/filename preferences can be fetched from a separate location if needed.
-    val previousHashStorePreferences: PreviousHashStorePreferences
-        get() = _previousHashStorePreferences
 
     @OptIn(ExperimentalStdlibApi::class)
     override suspend fun getItem(address: ByteArray): ByteArray? {
@@ -89,16 +87,16 @@ class PreviousHashStorePrefsRepository(
         private var INSTANCE: PreviousHashStorePrefsRepository? = null
 
         // Method to retrieve the singleton instance
-        fun getInstance(context: Context, previousHashStorePreferences: PreviousHashStorePreferences = defaultXyoSdkSettings.previousHashStorePreferences): PreviousHashStorePrefsRepository {
+        fun getInstance(context: Context, settings: SettingsInterface): PreviousHashStorePrefsRepository {
             val newInstance = INSTANCE ?: synchronized(this) {
-                INSTANCE ?: PreviousHashStorePrefsRepository(context.applicationContext, previousHashStorePreferences).also { INSTANCE = it }
+                INSTANCE ?: PreviousHashStorePrefsRepository(context.applicationContext, settings).also { INSTANCE = it }
             }
             return newInstance
         }
 
-        fun refresh(context: Context, previousHashStorePreferences: PreviousHashStorePreferences): PreviousHashStorePrefsRepository {
+        fun refresh(context: Context, settings: SettingsInterface): PreviousHashStorePrefsRepository {
             synchronized(this) {
-                INSTANCE = PreviousHashStorePrefsRepository(context.applicationContext, previousHashStorePreferences)
+                INSTANCE = PreviousHashStorePrefsRepository(context.applicationContext, settings)
                 return INSTANCE!!
             }
         }
