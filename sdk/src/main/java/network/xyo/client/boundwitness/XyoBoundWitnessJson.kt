197121951846d41b292fdcf42d97060afd767b96
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-11-25 16:49:22 -0500

    serializable meta object at top level with hash methods

--- sdk/src/main/java/network/xyo/client/boundwitness/XyoBoundWitnessJson.kt
+++ sdk/src/main/java/network/xyo/client/boundwitness/XyoBoundWitnessJson.kt
@@ -1,14 +1,33 @@
 package network.xyo.client.boundwitness
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-open class XyoBoundWitnessJson: XyoBoundWitnessBodyJson(), XyoBoundWitnessMetaInterface {
-    override var _signatures: List<String>? = null
-    override var _client: String? = null
-    override var _hash: String? = null
+class XyoBoundWitnessMeta: XyoBoundWitnessMetaInterface {
+    override var signatures: List<String>? = null
+    override var client: String? = null
+    override var hash: String? = null
+}
+
+@JsonClass(generateAdapter = true)
+open class XyoBoundWitnessJson: XyoBoundWitnessBodyJson() {
+    val _meta: XyoBoundWitnessMeta = XyoBoundWitnessMeta()
+
+    @Json(name = "\$meta")
+    var meta: XyoBoundWitnessMeta
+        get() = _meta
+        set(value) = Unit
+
+    fun rootHash(): String {
+        return sha256String(this)
+    }
+
+    fun dataHash(): String {
+        return sha256String(getBodyJson())
+    }
 
     open fun getBodyJson(): XyoBoundWitnessBodyJson {
-        return this
+        return XyoBoundWitnessBodyJson(this.addresses, this.previous_hashes, this.payload_hashes, this.payload_schemas, this.timestamp)
     }
 }
