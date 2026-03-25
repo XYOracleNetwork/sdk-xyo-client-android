fddf00d2bb7b000f4353239a7b500fd7a19f7c23
Arie Trouw <arie.trouw@xylabs.com>
2026-03-24 20:09:04 -0700

    Deploy 3.1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val majorVersion by extra(3)
 val minorVersion by extra(1)
-val patchVersion by extra(5)
+val patchVersion by extra(6)
 
 plugins {
     alias(libs.plugins.android.library) apply false
