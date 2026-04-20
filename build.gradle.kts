e5ddde1109374dcba31289ef98e6e66b11fc6c6a
Arie Trouw <6313781+arietrouw@users.noreply.github.com>
2026-04-20 11:00:28 -0700

    Deploy 3.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val majorVersion by extra(3)
 val minorVersion by extra(1)
-val patchVersion by extra(9)
+val patchVersion by extra(10)
 
 plugins {
     alias(libs.plugins.android.library) apply false
